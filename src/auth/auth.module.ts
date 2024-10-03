e9bdf360cc9c90ee8cfd542a86ee8796f89ebefd
Viktor <ogivita11q@gmail.com>
2024-10-03 20:13:51 +0300

    fixes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/user.module';
+import { PassportModule } from '@nestjs/passport';
+import TelegramStrategy from 'passport-telegram-official';
 
 @Module({
-  providers: [AuthService],
+  imports: [PassportModule, UsersModule], 
   controllers: [AuthController],
+  providers: [TelegramStrategy, AuthService],
 })
 export class AuthModule {}
