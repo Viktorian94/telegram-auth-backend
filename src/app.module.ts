297ab8595a48d2ed952c4caedf458f1a731c0125
Viktor <ogivita11q@gmail.com>
2024-10-06 14:34:02 +0300

    -_-

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TelegrafModule } from 'nestjs-telegraf';
 
+const port = process.env.DB_PORT as unknown as number;
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
@@ -15,10 +16,11 @@ import { TelegrafModule } from 'nestjs-telegraf';
       }),
       inject: [ConfigService],
     }),
+    
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
+      port: port || 3000,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
